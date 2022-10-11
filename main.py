5146194b3141b84cd8ad0141577d4ecdc5fe54d0
Your Name <you@example.com>
2022-10-10 23:36:53 -0700

    code

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 
 inputvalues = input('Enter all elements values: ')
 numbers1 = inputvalues.split() 
-for i in range(len(numbers1)):
-	numbers1[i] = int(numbers1[i]) 
-# The following line is the same as the for-loop
-# numbers1 = list(map(int, numbers))
-
-# print ("The original list: ", numbers1)
-
-# ******************************
-# Make your Code
-# ******************************
+numbers2=[]
+numbers3=[]
+x=len(numbers1)
+i=0
+y=0
+z=0
+while i<x:
+	numbers1[i] = int(numbers1[i])
+	i=i+1
+while y<x:
+	if y%2==0:
+		numbers2.append(numbers1[y])
+	y=y+1
+while z<x:
+	if z%2==1:
+		numbers3.append(numbers1[z])
+	z=z+1
+print("The list numbers:",numbers3)
+print("The list for even index elements",numbers2)
